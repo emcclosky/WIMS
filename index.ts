ec95c3666342f9324b72aebf52fa1e89387f56f7
Elizabeth Arinder <elizabeth.m.arinder@gmail.com>
2022-01-22 14:46:01 -0500

    Scraper: xpath match to button to check stock status

--- index.ts
+++ index.ts
@@ -2,9 +2,19 @@ import puppeteer from "puppeteer";
 
 const products = [
 	{
-		url: "https://shop.lululemon.com/p/womens-leggings/Unlimit-HighRise-Tight-25/_/prod9960879?color=30210&sz=4",
+		url: "https://shop.lululemon.com/p/womens-leggings/Wunder-Train-HR-Tight-25/_/prod9750562?color=47870&sz=4",
 		inStock: null,
-		keyword: "out of stock",
+		keyword: "Add to Bag",
+	},
+	{
+		url: "https://shop.lululemon.com/p/women-crops/Swift-Speed-High-Rise-Crop-23/_/prod10520368?color=0001&sz=4",
+		inStock: null,
+		keyword: "Add to Bag",
+	},
+	{
+		url: "https://www.garagegrowngear.com/collections/mens-clothing/products/mens-torrid-apex-jacket-by-enlightened-equipment?variant=15844953817162",
+		inStock: null,
+		keyword: "Sold out",
 	},
 ];
 
@@ -17,14 +27,17 @@ type Product = {
 type Products = Product[];
 
 const getInStockProducts = async (products: Products) => {
-	try {
-		const browser = await initPuppeteer();
+	const browser = await initPuppeteer();
 
+	try {
 		for (const product of products) {
 			product.inStock = await keywordPresent(browser, product);
 		}
 	} catch (err) {
 		console.error("error:", err);
+	} finally {
+		console.log("products", products);
+		await browser.close();
 	}
 };
 
@@ -36,8 +49,10 @@ const initPuppeteer = async () => {
 const keywordPresent = async (browser: puppeteer.Browser, product: Product) => {
 	const page = await browser.newPage();
 	await page.goto(product.url);
-	const html = await page.content();
-	return !html.toLowerCase().match(product.keyword);
+
+	return !(
+		await page.$x(`//button[contains(., '${product.keyword}')][@disabled]`)
+	).length;
 };
 
 // takes bool and filters for only in stock
