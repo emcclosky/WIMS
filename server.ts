efce5cbb57a68267e6c9f581334982a8b4e448a4
Elizabeth Arinder <elizabeth.m.arinder@gmail.com>
2022-01-22 21:38:16 -0500

    Server impl

---
+++ server.ts
@@ -0,0 +1,22 @@
+require("dotenv").config();
+import express from "express";
+import { getAllProducts } from "./db";
+
+const app = express();
+app.use(express.json());
+
+app.get("/products", async (_, res) => {
+	try {
+		const result = await getAllProducts();
+		return res.send(result).status(200);
+	} catch (err) {
+		console.error(err);
+		return res.sendStatus(500);
+	}
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}.`);
+});
